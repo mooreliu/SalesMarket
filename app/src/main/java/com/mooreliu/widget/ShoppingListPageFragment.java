79533bfdb2d661689875db25e68128bbe49f69cc
mooreliu <286210227@qq.com>
2015-09-28 18:25:24 +0800

    1 add License to README.md

--- app/src/main/java/com/mooreliu/widget/ShoppingListPageFragment.java
+++ app/src/main/java/com/mooreliu/widget/ShoppingListPageFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ * Created by liuyi on 15/8/29.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mooreliu.widget;
 
 import android.app.Activity;
@@ -9,9 +24,6 @@ import com.mooreliu.sync.NotifyInfo;
 import com.mooreliu.util.LogUtil;
 import com.mooreliu.view.InternetOffLayout;
 
-/**
- * Created by liuyi on 15/8/29.
- */
 public class ShoppingListPageFragment extends BaseObserverFragment {
 	private static final String TAG = "ShoppingListPageFragment";
 
